e52cb7c8f5b09164e3d73ec1855afe3978ddec72
Michael Pato <mtpato@gmail.com>
2012-06-30 18:11:11 -0400

    added server and tempclient classes

---
+++ src/gameProject/GameServer.java
@@ -0,0 +1,13 @@
+package gameProject;
+
+public class GameServer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/gameProject/TempClient.java
@@ -0,0 +1,13 @@
+package gameProject;
+
+public class TempClient {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
