4c304693d64ed5a8be2432e31f71d268153fa5d1
Michael Pato <mtpato@gmail.com>
2012-06-30 20:04:42 -0400

    made the Tile model observable

--- src/gameProject/TileModel.java
+++ src/gameProject/TileModel.java
@@ -1,5 +1,17 @@
 package gameProject;
 
-public class TileModel {
+import java.util.Observable;
+
+
+
+/**
+ * @author michaelpato
+ *
+ *this is the model it is observable and will alert its observers when if is changed
+ *these observers will be the alertThreads for now 
+ *
+ *
+ */
+public class TileModel extends Observable{
 
 }
