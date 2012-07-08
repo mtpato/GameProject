2ea55897cd1c3ca98652ee9f33ec180abf58fef2
Michael Pato <mtpato@gmail.com>
2012-07-08 17:34:00 -0400

    finished the GameModel class for now

--- src/gameProject/GameModel.java
+++ src/gameProject/GameModel.java
@@ -1,5 +1,7 @@
 package gameProject;
 
+import java.util.Set;
+
 public abstract class GameModel {
 	
 	/**
@@ -11,7 +13,7 @@ public abstract class GameModel {
 	 * @param move
 	 * @return the new state of the game after the move
 	 */
-	protected abstract String makeMove(String state, String move);
+	protected abstract String makeMove(GameState state, String move);
 	
 	/**
 	 * given the state of a game it returns if the game is over 
@@ -19,9 +21,56 @@ public abstract class GameModel {
 	 * @param state
 	 * @return whether the game is over 
 	 */
-	protected abstract boolean isOver(String state);
+	protected abstract boolean isOver(GameState state);
 	
-	protected abstract String makeMove(String state, String move);
+	/**
+	 * given the string that represents the state 
+	 * this function creates the game state object for the game that is 
+	 * being played
+	 * 
+	 * @param state
+	 * @return the parsed GameState
+	 */
+	protected abstract GameState parseGameState(String s);
+	
+	/**
+	 * given a game state it compresses it in the the string that represents 
+	 * the state for storage and communication with the client
+	 * 
+	 * @param s
+	 * @return String  the compress GameState
+	 */
+	protected abstract String compressGameState(GameState state);
+	
+	/**
+	 * returns the userID of the player that won the game 
+	 * 
+	 * @param state
+	 * @return the userID of the winner
+	 */
+	protected abstract int winner(GameState state);
+	
+	
+	/**
+	 * given a set of userIDs this function creates a game with those users
+	 * 
+	 * @param users
+	 * @return the GameState
+	 */
+	protected abstract GameState createNewGame(Set<Integer> users);
+	
+	
+	
+	/**
+	 * given a set of users and a game type (this is where things like difficulty 
+	 * or level type or who knows what else could be specified). it creates a
+	 * new game and returns the GameState object that represents that game;
+	 * 
+	 * @param users
+	 * @param type
+	 * @return the new GameState
+	 */
+	protected abstract GameState createNewGame(Set<Integer> users, String type);
 	
 
 }
--- src/gameProject/TileModel.java
+++ src/gameProject/TileModel.java
@@ -11,18 +11,6 @@ public class TileModel extends GameModel{
 
 
 
-	@Override
-	protected String makeMove(String state, String move) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	protected boolean isOver(String state) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-	
 	
 	
 }
