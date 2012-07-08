c29c987d6d7ce54a30fc07587230d125845ded2b
Michael Pato <mtpato@gmail.com>
2012-07-08 17:34:46 -0400

    added the unimplemented methods for the TileModel

--- src/gameProject/TileModel.java
+++ src/gameProject/TileModel.java
@@ -1,5 +1,7 @@
 package gameProject;
 
+import java.util.Set;
+
 
 /**
  * @author michaelpato
@@ -8,6 +10,48 @@ package gameProject;
  *
  */
 public class TileModel extends GameModel{
+
+	@Override
+	protected String makeMove(GameState state, String move) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected boolean isOver(GameState state) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected GameState parseGameState(String s) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String compressGameState(GameState state) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected int winner(GameState state) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected GameState createNewGame(Set<Integer> users) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected GameState createNewGame(Set<Integer> users, String type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 
 
