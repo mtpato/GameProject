4895cf953e968d73fc3993523e49faf1c6baf7eb
Michael Pato <mtpato@gmail.com>
2012-07-08 20:15:02 -0400

    just added some notes for tomorrow

--- src/gameProject/TileModel.java
+++ src/gameProject/TileModel.java
@@ -50,31 +50,11 @@ public class TileModel extends GameModel{
 		
 		
 		
-		int n = 1;
+		createTiles(state);
 		
-		for(int i = 0; i < 5; i ++) {
-			if(i % 2 == 0) {
-				for(int j = 0; j < 3; j++) {
-					state.tiles.put("" + i + "" + j, new TileNode(n + 10, i, j));
-					n++;
-					
-				}
-				
-				
-			} else {
-				
-				for(int j = 0; j < 4; j++) {
-					state.tiles.put("" + i + "" + j, new TileNode(n + 10, i, j));
-					n++;
-					
-				}
-				
-			}
-			
-			
-		}
+		//next need to set up adjs for the tiles 
 		
-		
+		//then GET THE RANDOM ASSIGNMENT OF TILES
 
 
 		
@@ -83,6 +63,7 @@ public class TileModel extends GameModel{
 		
 		return state;
 	}
+
 
 	@Override
 	protected GameState createNewGame(Set<Integer> users, String type) {
@@ -137,6 +118,32 @@ public class TileModel extends GameModel{
 		}
 		
 		
+	}
+	
+	private void createTiles(TileGameState state) {
+		int n = 1;
+		
+		for(int i = 0; i < 5; i ++) {
+			if(i % 2 == 0) {
+				for(int j = 0; j < 3; j++) {
+					state.tiles.put("" + i + "" + j, new TileNode(n + 10, i, j));
+					n++;
+					
+				}
+				
+				
+			} else {
+				
+				for(int j = 0; j < 4; j++) {
+					state.tiles.put("" + i + "" + j, new TileNode(n + 10, i, j));
+					n++;
+					
+				}
+				
+			}
+			
+			
+		}
 	}
 
 	
