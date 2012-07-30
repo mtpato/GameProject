2daca21e419e410d987377f7f1ba8c8de01021fd
Michael Pato <mtpato@gmail.com>
2012-07-30 16:19:42 -0400

    updated the new IP now that i moved and set up the server. also removed override in the TileNodeComparator

--- src/gameProject/TileNodeComparator.java
+++ src/gameProject/TileNodeComparator.java
@@ -4,7 +4,7 @@ import java.util.Comparator;
 
 public class TileNodeComparator implements Comparator{
 
-	@Override
+	
 	public int compare(Object arg0, Object arg1) {
 		
 		return ((TileNode) arg0).nodeID - ((TileNode) arg1).nodeID;
